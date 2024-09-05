f75dba929cf08709bd5dc94b3bd25c07720fee70
Valerii Eronin <ervaler2004@gmail.com>
2024-09-05 11:33:46 +0300

    Main by default starts an endless game that interacts with the user via the console

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,13 @@
 package backend.academy;
 
+import backend.academy.hangman.game.Game;
+import backend.academy.hangman.player.HumanPlayer;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        Game game = new Game(new HumanPlayer());
+        game.endlessPlay();
     }
 }
